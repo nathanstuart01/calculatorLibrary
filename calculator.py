b03ab42b2d76e66e66d16419474eefdce43ffecc
Nathan Stuart <nathan.stuart@ascentialedge.com>
2019-07-22 16:30:40 -0600

    added linting fixes

--- calculator.py
+++ calculator.py
@@ -10,5 +10,6 @@ def add(first_term, second_term):
 def subtract(first_term, second_term):
     return first_term - second_term
 
+
 def multiple(first_term, second_term):
-	return first_term * second_term
+    return first_term * second_term
